be0378b6cd63c237f60731b98ecc8899fd8f4dad
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-02-02 09:20:55 -0500

    Add weekday and month labels to sliders, change severity colors

--- CustomSlider/CustomSlider/ContentView.swift
+++ CustomSlider/CustomSlider/ContentView.swift
@@ -9,14 +9,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    let days = [Date().addingTimeInterval(-60 * 60 * 48),
+                Date().addingTimeInterval(-60 * 60 * 24),
+                Date(),
+                Date().addingTimeInterval(60 * 60 * 24),
+                Date().addingTimeInterval(60 * 60 * 48),
+                Date().addingTimeInterval(60 * 60 * 72),
+                Date().addingTimeInterval(60 * 60 * 96)]
+    
     var body: some View{
-        StorySliderGrid(days: Array(1...7))
+        StorySliderGrid(days: days)
     }
 }
 
 struct StorySliderGrid: View {
-    let days: [Int]
-    @State var values: [Int: String] = [:]
+    let days: [Date]
+    @State var values: [Date: String] = [:]
     
     var body: some View{
         ZStack(){
@@ -68,7 +76,7 @@ struct StorySliderGrid: View {
             ScrollView(.horizontal, showsIndicators: false){
                 HStack(){
                     ForEach(self.days, id: \.self){ day in
-                        StoryDaySlider(values: self.$values, day: day).padding(10)
+                        StoryDaySlider(values: self.$values, day: day).padding(4)
                     }
                 }
                 .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
@@ -81,12 +89,13 @@ struct StorySliderGrid: View {
     }
 }
 
+
 struct StoryDaySlider: View {
-    @Binding var values: [Int: String]
+    @Binding var values: [Date: String]
     @State private var position = CGSize(width: 0, height: 100)
     @GestureState private var dragOffset = CGSize.zero
     @State private var buttonColor = Color.gray
-    let day: Int
+    let day: Date
     
     func updateSeverity(severity: String){
         self.values[self.day] = severity
@@ -96,19 +105,19 @@ struct StoryDaySlider: View {
         switch position {
         case "severe":
             self.position.height = -300
-            self.buttonColor = Color.red
+            self.buttonColor = Color(UIColor(red:1.00, green:0.47, blue:0.47, alpha:1.0))
             self.updateSeverity(severity: "severe")
         case "moderate":
             self.position.height = -200
-            self.buttonColor = Color.orange
+            self.buttonColor = Color(UIColor(red:1.00, green:0.75, blue:0.46, alpha:1.0))
             self.updateSeverity(severity: "moderate")
         case "mild":
             self.position.height = -100
-            self.buttonColor = Color.yellow
+            self.buttonColor = Color(UIColor(red:0.96, green:0.90, blue:0.55, alpha:1.0))
             self.updateSeverity(severity: "mild")
         case "none":
             self.position.height = 0
-            self.buttonColor = Color.green
+            self.buttonColor = Color(UIColor(red:0.73, green:0.86, blue:0.35, alpha:1.0))
             self.updateSeverity(severity: "none")
         default:
             self.position.height = 100
@@ -116,6 +125,27 @@ struct StoryDaySlider: View {
             self.updateSeverity(severity: "null")
         }
         print("day: \(self.day), value: \(self.values[self.day]!)")
+    }
+    
+    func getDayfromDate(date: Date) -> Int{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd"
+        let dayString = formatter.string(from: date)
+        return Int(dayString) ?? 0
+        
+    }
+    
+    func getWeekdayfromDate(date: Date) -> String{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        let weekDay = dateFormatter.string(from: date)
+        return String(weekDay.prefix(3))
+    }
+    
+    func getMonthfromDate(date: Date) -> String{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM"
+        return dateFormatter.string(from: date)
     }
     
     func ordinalNumber(num: Int) -> String {
@@ -127,16 +157,25 @@ struct StoryDaySlider: View {
     var body: some View {
         ZStack(){
             Group(){
-            Rectangle()
-                .fill(Color(UIColor.lightGray))
-                .frame(width: 1, height: 300)
-                .opacity(0.3)
-                .offset(y: -150)
-            Rectangle()
-                .fill(Color(UIColor.lightGray))
-                .frame(width: 1, height: 100)
-                .opacity(0.3)
-                .offset(y: 50)
+                Rectangle()
+                    .fill(Color(UIColor.lightGray))
+                    .frame(width: 1, height: 300)
+                    .opacity(0.3)
+                    .offset(y: -150)
+                Rectangle()
+                    .fill(Color(UIColor.lightGray))
+                    .frame(width: 1, height: 100)
+                    .opacity(0.3)
+                    .offset(y: 50)
+                Text(getMonthfromDate(date: self.day))
+                    .font(.custom("AvenirNext-Regular", size: 18))
+                    .foregroundColor((Color(red: 33/255, green: 61/255, blue: 153/255)))
+                    .opacity(0.5)
+                    .offset(y: 175)
+                Text(getWeekdayfromDate(date: self.day))
+                    .font(.custom("AvenirNext-Regular", size: 18))
+                    .foregroundColor((Color(red: 33/255, green: 61/255, blue: 153/255)))
+                    .offset(y: 150)
             }
             Group(){
                 Button(action: {
@@ -163,14 +202,16 @@ struct StoryDaySlider: View {
                     Circle().frame(width:10, height: 10).foregroundColor(Color(UIColor.lightGray))
                 }.offset(y: -300)
             }
-            Text(ordinalNumber(num: self.day))
+            Text(String(getDayfromDate(date: self.day)))
                 .frame(width: 50, height: 50)
                 .foregroundColor(Color.white)
                 .font(.custom("AvenirNext-Regular", size: 22))
-                .background(Circle().fill(self.buttonColor))
+                .background(
+                    Circle()
+                        .fill(self.buttonColor))
                 .padding(5)
                 .offset(x: 0, y: self.position.height + self.dragOffset.height)
-                .animation(.easeInOut)
+                .animation(.easeOut)
                 .gesture(
                     DragGesture()
                         .updating(self.$dragOffset, body: { (value, state, transaction) in
